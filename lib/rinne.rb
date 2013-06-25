b0b22c9c99b4ed6c166382cc1b5ca2c0640c2040
Naoto SHINGAKI <nao.sora+github@gmail.com>
2013-06-25 17:07:15 +0900

    add snake case mode

--- lib/rinne.rb
+++ lib/rinne.rb
@@ -15,6 +15,13 @@ module Rinne
     def classify(path, filetype = "*")
       camelize File.basename(path, ".#{filetype}")
     end
+
+    def to_snake(str)
+      str.gsub(/::/, '/').
+        gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+        gsub(/([a-z\d])([A-Z])/,'\1_\2').
+        tr("-","_").downcase
+    end
   end
 
 end
